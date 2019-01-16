372ac7845f5911f1778b91e43945754468599de2
Lain Matsuoka <lain21@mercari.com>
2019-01-16 17:43:22 +0900

    bug fixed

--- src/model/baseline_rnn.py
+++ src/model/baseline_rnn.py
@@ -85,8 +85,8 @@ class StackedRNNFastFM(nn.Module):
         self.lstm_attention = Attention(hidden_size * 2, seq_len)
         self.gru_attention = Attention(hidden_size * 2, seq_len)
 
-        fm_first_size = hidden_size * 2 * 4
-        fm_second_size = hidden_size * 2 * sp.special.comb(4, 2)
+        fm_first_size = hidden_size * 2 * 5
+        fm_second_size = hidden_size * 2 * sp.special.comb(5, 2)
 
         self.fc = nn.Linear(int(fm_first_size + fm_second_size), out_hidden_dim)
         self.relu = nn.ReLU()
