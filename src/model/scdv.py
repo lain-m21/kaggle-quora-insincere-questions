cbbb1d2663fb92fbb10b760f200cb1a0cfb68380
Lain Matsuoka <lain21@mercari.com>
2019-01-16 23:59:35 +0900

    SCDV and its test

---
+++ src/model/scdv.py
@@ -0,0 +1,106 @@
+import numpy as np
+from sklearn.mixture import GaussianMixture
+from sklearn.feature_extraction.text import TfidfVectorizer
+from keras.preprocessing.text import text_to_word_sequence
+
+
+class SCDV:
+    def __init__(self, embedding_matrix: np.ndarray, tokenizer, logger, num_clusters=50):
+        self.embedding_matrix = embedding_matrix
+        self.tokenizer = tokenizer
+        self.num_clusters = num_clusters
+        self.logger = logger
+        self.min_no = 0
+        self.max_no = 0
+
+        self.word_centroid_map = None
+        self.word_centroid_prob_map = None
+        self.word_idf_dict = None
+        self.prob_wordvecs = None
+        self.threshold = 0
+
+    def fit_transform(self, texts, sequences):
+        idx, idx_proba = self._cluster_gmm()
+
+        self.logger.info('Form word centroid maps')
+        self.word_centroid_map = dict(zip(list(range(self.embedding_matrix.shape[1])), idx))
+        self.word_centroid_prob_map = dict(zip(list(range(self.embedding_matrix.shape[1])), idx_proba))
+
+        self.logger.info('Fit TfidfVectorizer on train text data')
+        vectorizer = TfidfVectorizer(dtype=np.float32, tokenizer=text_to_word_sequence)
+        vectorizer.fit(texts)
+        feature_names = vectorizer.get_feature_names()
+        idf_list = vectorizer._tfidf.idf_
+
+        self.logger.info('Form word idf dictionary')
+        self.word_idf_dict = dict(zip([self.tokenizer.word_index[w] for w in feature_names], idf_list))
+
+        self.logger.info('Compute probability word vectors')
+        self.prob_wordvecs = self._get_probability_word_vectors(self.word_centroid_map,
+                                                                self.word_centroid_prob_map,
+                                                                self.word_idf_dict)
+
+        self.logger.info('Compute SCDV vector for each text')
+        scdv_vectors = np.zeros([len(sequences), self.num_clusters * self.embedding_matrix.shape[1]])
+        for i, seq in enumerate(sequences):
+            scdv_vectors[i] = self._compute_scdv_vector(seq, self.prob_wordvecs, self.word_centroid_map, train=True)
+
+        self.threshold = 0.04 * (abs(self.min_no / len(sequences)) + abs(self.max_no / len(sequences))) / 2
+        self.logger.info(f'Threshold: {self.threshold}')
+
+        scdv_vectors[scdv_vectors < self.threshold] = 0
+
+        return scdv_vectors
+
+    def transform(self, sequences):
+        self.logger.info('Compute SCDV vector for each text')
+        scdv_vectors = np.zeros([len(sequences), self.num_clusters * self.embedding_matrix.shape[1]])
+        for i, seq in enumerate(sequences):
+            scdv_vectors[i] = self._compute_scdv_vector(seq, self.prob_wordvecs, self.word_centroid_map, train=False)
+
+        scdv_vectors[scdv_vectors < self.threshold] = 0
+
+        return scdv_vectors
+
+    def _cluster_gmm(self):
+        mixture_model = GaussianMixture(n_components=self.num_clusters,
+                                        covariance_type='tied',
+                                        init_params='kmeans',
+                                        max_iter=50)
+        self.logger.info('Fit Gaussian Mixture model...')
+        mixture_model.fit(self.embedding_matrix)
+        self.logger.info('Cluster word vectors')
+        idx = mixture_model.predict(self.embedding_matrix)
+        idx_proba = mixture_model.predict_proba(self.embedding_matrix)
+        return idx, idx_proba
+
+    def _get_probability_word_vectors(self, word_centroid_map, word_centroid_prob_map, word_idf_dict):
+        prob_wordvecs = {}
+        num_features = self.embedding_matrix.shape[1]
+        for word_id in word_centroid_map:
+            prob_wordvecs[word_id] = np.zeros(self.num_clusters * num_features, dtype="float32")
+            for index in range(self.num_clusters):
+                prob_wordvecs[word_id][index * num_features:(index + 1) * num_features] = \
+                    self.embedding_matrix[word_id] * \
+                    word_centroid_prob_map[word_id][index] * \
+                    word_idf_dict[word_id]
+
+        return prob_wordvecs
+
+    def _compute_scdv_vector(self, sequence, prob_wordvecs, word_centroid_map, train=True):
+        scdv_vector = np.zeros(self.num_clusters * self.embedding_matrix.shape[1], dtype="float32")
+
+        for word_id in sequence:
+            if word_id in word_centroid_map:
+                scdv_vector += prob_wordvecs[word_id]
+
+        norm = np.sqrt(np.einsum('...i,...i', scdv_vector, scdv_vector))
+        if norm != 0:
+            scdv_vector /= norm
+
+        # To make feature vector sparse, make note of minimum and maximum values.
+        if train:
+            self.min_no += scdv_vector.min()
+            self.max_no += scdv_vector.max()
+
+        return scdv_vector
---
+++ src/tests/test_scdv.py
@@ -0,0 +1,45 @@
+import json
+from pathlib import Path
+
+from model.scdv import SCDV
+from utils.prepare_data import load_data, preprocess_text, tokenize_text, load_embeddings
+from utils.logger import Logger
+
+INPUT_DIR = Path.cwd().joinpath('../../input')
+DATA_DIR = Path.cwd().joinpath('../../data')
+LOG_DIR = Path.cwd().joinpath('../../logs')
+SLACK_URL = json.load(Path.cwd().joinpath('../config.json').open('r'))['slack']
+
+
+def main(logger):
+    df_train, df_test = load_data(INPUT_DIR, logger)
+    logger.info('Preprocess text')
+    df_train = preprocess_text(df_train)
+    df_test = preprocess_text(df_test)
+    seq_train, tokenizer = tokenize_text(df_train, logger)
+    seq_test, _ = tokenize_text(df_test, logger, tokenizer=tokenizer)
+    text_train = df_train['question_text'].values.tolist()
+    embedding_matrix = load_embeddings(0, tokenizer.word_index)
+
+    scdv = SCDV(embedding_matrix, tokenizer, logger, num_clusters=50)
+    with logger.timer('SCDV computation on train data'):
+        scdv_train = scdv.fit_transform(text_train, seq_train)
+
+    logger.post(f'Computing SCDV for train data has been done: shape = {scdv_train.shape}')
+
+    with logger.timer('SCDV computation on test data'):
+        scdv_test = scdv.transform(seq_test)
+
+    logger.post(f'Computing SCDV for test data has been done: shape = {scdv_test.shape}')
+
+
+if __name__ == '__main__':
+    script_name = Path(__file__).stem
+    log_dir = LOG_DIR.joinpath(f'{script_name}')
+    logger = Logger(script_name, log_dir=log_dir, webhook_url=SLACK_URL, overwrite=True)
+    logger.info(f'Script starts with command line arguments: {args}')
+    try:
+        main(logger)
+        logger.post('===== Script completed successfully! =====')
+    except Exception as e:
+        logger.exception(e)
