501231ec52b3117fa911a98435dc698c00134775
Lain Matsuoka <lain21@mercari.com>
2019-01-16 21:15:07 +0900

    NLPFeaturesRNNFM

---
+++ src/model/nlprnn.py
@@ -0,0 +1,134 @@
+import itertools
+import scipy as sp
+import torch
+import torch.nn as nn
+
+from .common import Attention, Dense
+
+
+class NLPFeaturesRNNFM(nn.Module):
+    def __init__(self, input_shapes, embedding_matrix, seq_len, hidden_size=64, out_hidden_dim=32,
+                 embed_drop=0.1, recurrent_drop=0.0, out_drop=0.2):
+        super(NLPFeaturesRNNFM, self).__init__()
+
+        self.embedding_continuous = nn.ModuleList([
+            Dense(1, hidden_size * 2, dropout=0.2) for _ in range(input_shapes['continuous'])
+        ])
+
+        self.embedding = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1])
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+
+        self.embedding_dropout = nn.Dropout2d(embed_drop)
+
+        self.lstm = nn.LSTM(embedding_matrix.shape[1], hidden_size, dropout=recurrent_drop,
+                            bidirectional=True, batch_first=True)
+        self.gru = nn.GRU(hidden_size * 2, hidden_size, dropout=recurrent_drop,
+                          bidirectional=True, batch_first=True)
+
+        self.lstm_attention = Attention(hidden_size * 2, seq_len)
+        self.gru_attention = Attention(hidden_size * 2, seq_len)
+
+        fm_first_size = hidden_size * 2 * (4 + len(self.embedding_continuous))
+        fm_second_size = hidden_size * 2 * sp.special.comb(4 + len(self.embedding_continuous), 2)
+
+        self.fc = nn.Linear(int(fm_first_size + fm_second_size), out_hidden_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(out_drop)
+        self.output_layer = nn.Linear(out_hidden_dim, 1)
+
+    def forward(self, inputs):
+        x_embedding = self.embedding(inputs['text'])  # B x L x D
+        x_embedding = self.embedding_dropout(torch.unsqueeze(x_embedding, 0).transpose(1, 3))
+        x_embedding = torch.squeeze(x_embedding.transpose(1, 3))
+
+        x_lstm, _ = self.lstm(x_embedding)
+        x_gru, _ = self.gru(x_lstm)
+
+        x_lstm_attention = self.lstm_attention(x_lstm)
+        x_gru_attention = self.gru_attention(x_gru)
+        x_avg_pool = torch.mean(x_gru, 1)
+        x_max_pool, _ = torch.max(x_gru, 1)
+
+        x_continuous = []
+        for x, layer in zip(inputs['continuous'], self.embedding_continuous):
+            x_continuous.append(layer(x))
+
+        fm_first = [
+            x_lstm_attention,
+            x_gru_attention,
+            x_avg_pool,
+            x_max_pool
+        ] + x_continuous
+
+        fm_second = []
+        for t_1, t_2 in itertools.combinations(fm_first, 2):
+            fm_second.append(t_1 * t_2)
+
+        x_fc = self.fc(torch.cat(fm_first + fm_second, 1))
+        x_fc = self.dropout(self.relu(x_fc))
+        outputs = self.output_layer(x_fc)
+        return outputs
+
+
+class StackedRNNFastFM(nn.Module):
+    def __init__(self, embedding_matrix, seq_len, hidden_size=64, out_hidden_dim=32,
+                 embed_drop=0.1, recurrent_drop=0.0, out_drop=0.2):
+        super(StackedRNNFastFM, self).__init__()
+
+        self.embedding = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1])
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+
+        self.embedding_dropout = nn.Dropout2d(embed_drop)
+
+        self.lstm = nn.LSTM(embedding_matrix.shape[1], hidden_size, dropout=recurrent_drop,
+                            bidirectional=True, batch_first=True)
+        self.gru = nn.GRU(hidden_size * 2, hidden_size, dropout=recurrent_drop,
+                          bidirectional=True, batch_first=True)
+
+        self.fast_attention = Attention(embedding_matrix.shape[1], seq_len)
+        self.fast_dropout = nn.Dropout(0.2)
+        self.fast_fc = nn.Linear(embedding_matrix.shape[1], hidden_size * 2)
+        self.lstm_attention = Attention(hidden_size * 2, seq_len)
+        self.gru_attention = Attention(hidden_size * 2, seq_len)
+
+        fm_first_size = hidden_size * 2 * 5
+        fm_second_size = hidden_size * 2 * sp.special.comb(5, 2)
+
+        self.fc = nn.Linear(int(fm_first_size + fm_second_size), out_hidden_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(out_drop)
+        self.output_layer = nn.Linear(out_hidden_dim, 1)
+
+    def forward(self, inputs):
+        x_embedding = self.embedding(inputs)  # B x L x D
+        x_embedding = self.embedding_dropout(torch.unsqueeze(x_embedding, 0).transpose(1, 3))
+        x_embedding = torch.squeeze(x_embedding.transpose(1, 3))
+
+        x_lstm, _ = self.lstm(x_embedding)
+        x_gru, _ = self.gru(x_lstm)
+
+        x_fast_attention = self.fast_attention(x_embedding)
+        x_fast_attention = self.fast_dropout(self.fast_fc(x_fast_attention))
+        x_lstm_attention = self.lstm_attention(x_lstm)
+        x_gru_attention = self.gru_attention(x_gru)
+        x_avg_pool = torch.mean(x_gru, 1)
+        x_max_pool, _ = torch.max(x_gru, 1)
+
+        fm_first = [
+            x_fast_attention,
+            x_lstm_attention,
+            x_gru_attention,
+            x_avg_pool,
+            x_max_pool
+        ]
+
+        fm_second = []
+        for t_1, t_2 in itertools.combinations(fm_first, 2):
+            fm_second.append(t_1 * t_2)
+
+        x_fc = self.fc(torch.cat(fm_first + fm_second, 1))
+        x_fc = self.dropout(self.relu(x_fc))
+        outputs = self.output_layer(x_fc)
+        return outputs
