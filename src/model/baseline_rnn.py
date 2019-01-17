3d0090f4a324503c6d964e00294f9d9a4667902c
Lain Matsuoka <lain21@mercari.com>
2019-01-17 12:15:19 +0900

    RNN with LayerNormalization and residual

--- src/model/baseline_rnn.py
+++ src/model/baseline_rnn.py
@@ -125,3 +125,68 @@ class StackedRNNFastFM(nn.Module):
         x_fc = self.dropout(self.relu(x_fc))
         outputs = self.output_layer(x_fc)
         return outputs
+
+
+class StackedNormalizedRNNFM(nn.Module):
+    def __init__(self, embedding_matrix, seq_len, hidden_size=64, out_hidden_dim=32,
+                 embed_drop=0.1, residual=False, out_drop=0.2):
+        super(StackedNormalizedRNNFM, self).__init__()
+
+        self.residual = residual
+
+        self.embedding = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1])
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+
+        self.embedding_dropout = nn.Dropout2d(embed_drop)
+
+        self.lstm = nn.LSTM(embedding_matrix.shape[1], hidden_size / 2, bidirectional=True, batch_first=True)
+        self.lstm_norm = nn.LayerNorm(hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, bidirectional=True, batch_first=True)
+        self.gru_norm = nn.LayerNorm(hidden_size)
+
+        self.lstm_attention = Attention(hidden_size, seq_len)
+        self.gru_attention = Attention(hidden_size, seq_len)
+
+        fm_first_size = hidden_size * 4
+        fm_second_size = hidden_size * sp.special.comb(4, 2)
+
+        self.fc = nn.Linear(int(fm_first_size + fm_second_size), out_hidden_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(out_drop)
+        self.output_layer = nn.Linear(out_hidden_dim, 1)
+
+    def forward(self, inputs):
+        x_embedding = self.embedding(inputs)  # B x L x D
+        x_embedding = self.embedding_dropout(torch.unsqueeze(x_embedding, 0).transpose(1, 3))
+        x_embedding = torch.squeeze(x_embedding.transpose(1, 3))
+
+        x_lstm, _ = self.lstm(x_embedding)
+        if self.residual:
+            x_lstm += x_embedding
+        x_lstm = self.lstm_norm(x_lstm)
+        x_gru, _ = self.gru(x_lstm)
+        if self.residual:
+            x_gru += x_lstm
+        x_gru = self.gru_norm(x_gru)
+
+        x_lstm_attention = self.lstm_attention(x_lstm)
+        x_gru_attention = self.gru_attention(x_gru)
+        x_avg_pool = torch.mean(x_gru, 1)
+        x_max_pool, _ = torch.max(x_gru, 1)
+
+        fm_first = [
+            x_lstm_attention,
+            x_gru_attention,
+            x_avg_pool,
+            x_max_pool
+        ]
+
+        fm_second = []
+        for t_1, t_2 in itertools.combinations(fm_first, 2):
+            fm_second.append(t_1 * t_2)
+
+        x_fc = self.fc(torch.cat(fm_first + fm_second, 1))
+        x_fc = self.dropout(self.relu(x_fc))
+        outputs = self.output_layer(x_fc)
+        return outputs
