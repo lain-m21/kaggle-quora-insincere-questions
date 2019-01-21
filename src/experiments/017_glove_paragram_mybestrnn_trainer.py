03ae9eaff7c343b90bc95d212e457b740c2f0207
Lain Matsuoka <lain21@mercari.com>
2019-01-21 17:33:38 +0900

    debugging finished!

--- src/experiments/017_glove_paragram_mybestrnn_trainer.py
+++ src/experiments/017_glove_paragram_mybestrnn_trainer.py
@@ -89,10 +89,10 @@ def main(logger, args):
             'scheduler_type': 'cyclic',
             'base_lr': 0.00001,
             'max_lr': 0.003,
-            'step_size': 240,
+            'step_size': 1200,
             'scheduler_mode': 'triangular',
             'scheduler_gamma': 0.9,
-            'scheduler_trigger_steps': 300,
+            'scheduler_trigger_steps': 4000,
             'sampler_type': 'normal',
             'seed': SEED
         }
