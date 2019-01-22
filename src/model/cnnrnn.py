f2f3d1ef4cc8e263d4685c522d23aad79e42a812
Lain Matsuoka <lain21@mercari.com>
2019-01-22 22:04:54 +0900

    branched cnn and its alternative

--- src/model/cnnrnn.py
+++ src/model/cnnrnn.py
@@ -3,7 +3,213 @@ import scipy as sp
 import torch
 import torch.nn as nn
 
-from .common import Attention
+from .common import Attention, GeneralAttention
+
+
+class BranchedMaskCNNRNN(nn.Module):
+    def __init__(self, embedding_matrix, hidden_size=64, out_hidden_dim=64, attention_type='general',
+                 kernel_sizes=(2, 7), embed_drop=0.2, out_drop=0.3, pool_type='avg', mask=False):
+        super(BranchedMaskCNNRNN, self).__init__()
+
+        self.pool_type = pool_type
+        self.mask = mask
+
+        self.embedding = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1])
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+
+        self.embedding_dropout = nn.Dropout2d(embed_drop)
+
+        self.lstm = nn.LSTM(embedding_matrix.shape[1], hidden_size, bidirectional=True, batch_first=True)
+        self.gru = nn.GRU(hidden_size * 2, hidden_size, bidirectional=True, batch_first=True)
+        self.cnn_layers = nn.ModuleList([nn.ModuleList([
+            nn.Conv1d(hidden_size * 2, hidden_size * 2, kernel_size=k),
+            nn.ReLU()
+        ]) for k in range(*kernel_sizes)])
+
+        self.lstm_attention = GeneralAttention(hidden_size * 2, attention_type)
+        self.gru_attention = GeneralAttention(hidden_size * 2, attention_type)
+        self.cnn_attention_layers = nn.ModuleList([GeneralAttention(hidden_size * 2, attention_type)])
+
+        if pool_type == 'both':
+            pool_factor = 2
+        else:
+            pool_factor = 1
+        fm_input_size = 4 + pool_factor * len(list(range(*kernel_sizes)))
+        fm_first_size = hidden_size * 2 * fm_input_size
+        fm_second_size = hidden_size * 2 * sp.special.comb(fm_input_size, 2)
+
+        self.fc = nn.Linear(int(fm_first_size + fm_second_size), out_hidden_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(out_drop)
+        self.output_layer = nn.Linear(out_hidden_dim, 1)
+
+    def forward(self, inputs):
+        if self.mask:
+            x_embedding = self.embedding(inputs['sequence'])  # B x L x D
+        else:
+            x_embedding = self.embedding(inputs)  # B x L x D
+        x_embedding = self.embedding_dropout(torch.unsqueeze(x_embedding, 0).transpose(1, 3))
+        x_embedding = torch.squeeze(x_embedding.transpose(1, 3))
+
+        x_lstm, _ = self.lstm(x_embedding)
+        x_gru, _ = self.gru(x_lstm)
+        x_cnn = []
+        for layers in self.cnn_layers:
+            x = layers[0](x_lstm.transpose(1, 2))
+            x = layers[1](x)
+            x_cnn.append(x.transpose(1, 2))
+
+        x_lstm_attention, _ = self.lstm_attention(x_lstm, x_lstm)
+        x_gru_attention, _ = self.gru_attention(x_gru, x_gru)
+        x_cnn_attention  = []
+        for x, layer in zip(x_cnn, self.cnn_attention_layers):
+            x_attention, _ = layer(x, x)
+            if self.mask:
+                x_attention = x_attention * inputs['mask'].unsqueeze(-1)
+            x_cnn_attention.append(x_attention)
+
+        if self.mask:
+            x_lstm_attention = x_lstm_attention * inputs['mask'].unsqueeze(-1)
+            x_gru_attention = x_gru_attention * inputs['mask'].unsqueeze(-1)
+
+        x_avg_pool_lstm = torch.mean(x_lstm_attention, 1)
+        x_max_pool_lstm, _ = torch.max(x_lstm_attention, 1)
+        x_avg_pool_gru = torch.mean(x_gru_attention, 1)
+        x_max_pool_gru, _ = torch.max(x_gru_attention, 1)
+
+        x_pool_cnn = []
+        for x in x_cnn_attention:
+            if self.pool_type == 'avg':
+                x_pool = torch.mean(x, 1)
+                x_pool_cnn.append(x_pool)
+            elif self.pool_type == 'max':
+                x_pool, _ = torch.max(x, 1)
+                x_pool_cnn.append(x_pool)
+            else:
+                x_avg_pool = torch.mean(x, 1)
+                x_max_pool, _ = torch.max(x, 1)
+                x_pool_cnn.append(x_avg_pool)
+                x_pool_cnn.append(x_max_pool)
+
+        fm_first = [
+            x_avg_pool_lstm,
+            x_max_pool_lstm,
+            x_avg_pool_gru,
+            x_max_pool_lstm
+        ] + x_pool_cnn
+
+        fm_second = []
+        for t_1, t_2 in itertools.combinations(fm_first, 2):
+            fm_second.append(t_1 * t_2)
+
+        x_fc = self.fc(torch.cat(fm_first + fm_second, 1))
+        x_fc = self.dropout(self.relu(x_fc))
+        outputs = self.output_layer(x_fc)
+        return outputs
+
+
+class BranchedMaskCNNRNNAnother(nn.Module):
+    def __init__(self, embedding_matrix, hidden_size=64, out_hidden_dim=64, attention_type='general',
+                 kernel_sizes=(2, 7), embed_drop=0.2, out_drop=0.3, pool_type='avg', mask=False):
+        super(BranchedMaskCNNRNNAnother, self).__init__()
+
+        self.pool_type = pool_type
+        self.mask = mask
+
+        self.embedding = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1])
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+
+        self.embedding_dropout = nn.Dropout2d(embed_drop)
+
+        self.lstm = nn.LSTM(embedding_matrix.shape[1], hidden_size, bidirectional=True, batch_first=True)
+        self.gru = nn.GRU(hidden_size * 2, hidden_size, bidirectional=True, batch_first=True)
+        self.cnn_layers = nn.ModuleList([nn.ModuleList([
+            nn.Conv1d(hidden_size * 2, hidden_size * 2, kernel_size=k),
+            nn.ReLU()
+        ]) for k in range(*kernel_sizes)])
+
+        self.lstm_attention = GeneralAttention(hidden_size * 2, attention_type)
+        self.gru_attention = GeneralAttention(hidden_size * 2, attention_type)
+        self.cnn_attention_layers = nn.ModuleList([GeneralAttention(hidden_size * 2, attention_type)])
+
+        if pool_type == 'both':
+            pool_factor = 2
+        else:
+            pool_factor = 1
+        fm_input_size = 4 + pool_factor * len(list(range(*kernel_sizes)))
+        fm_first_size = hidden_size * 2 * fm_input_size
+        fm_second_size = hidden_size * 2 * sp.special.comb(fm_input_size, 2)
+
+        self.fc = nn.Linear(int(fm_first_size + fm_second_size), out_hidden_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(out_drop)
+        self.output_layer = nn.Linear(out_hidden_dim, 1)
+
+    def forward(self, inputs):
+        if self.mask:
+            x_embedding = self.embedding(inputs['sequence'])  # B x L x D
+        else:
+            x_embedding = self.embedding(inputs)  # B x L x D
+        x_embedding = self.embedding_dropout(torch.unsqueeze(x_embedding, 0).transpose(1, 3))
+        x_embedding = torch.squeeze(x_embedding.transpose(1, 3))
+
+        x_lstm, _ = self.lstm(x_embedding)
+        x_lstm, _ = self.lstm_attention(x_lstm, x_lstm)
+        if self.mask:
+            x_lstm = x_lstm * inputs['mask'].unsqueeze(-1)
+        x_gru, _ = self.gru(x_lstm)
+        x_gru, _ = self.gru_attention(x_gru, x_gru)
+        if self.mask:
+            x_gru = x_gru * inputs['mask'].unsqueeze(-1)
+        x_cnn = []
+        for layers in self.cnn_layers:
+            x = layers[0](x_lstm.transpose(1, 2))
+            x = layers[1](x)
+            x_cnn.append(x.transpose(1, 2))
+
+        x_cnn_attention  = []
+        for x, layer in zip(x_cnn, self.cnn_attention_layers):
+            x_attention, _ = layer(x, x)
+            if self.mask:
+                x_attention = x_attention * inputs['mask'].unsqueeze(-1)
+            x_cnn_attention.append(x_attention)
+
+        x_avg_pool_lstm = torch.mean(x_lstm, 1)
+        x_max_pool_lstm, _ = torch.max(x_lstm, 1)
+        x_avg_pool_gru = torch.mean(x_gru, 1)
+        x_max_pool_gru, _ = torch.max(x_gru, 1)
+
+        x_pool_cnn = []
+        for x in x_cnn_attention:
+            if self.pool_type == 'avg':
+                x_pool = torch.mean(x, 1)
+                x_pool_cnn.append(x_pool)
+            elif self.pool_type == 'max':
+                x_pool, _ = torch.max(x, 1)
+                x_pool_cnn.append(x_pool)
+            else:
+                x_avg_pool = torch.mean(x, 1)
+                x_max_pool, _ = torch.max(x, 1)
+                x_pool_cnn.append(x_avg_pool)
+                x_pool_cnn.append(x_max_pool)
+
+        fm_first = [
+            x_avg_pool_lstm,
+            x_max_pool_lstm,
+            x_avg_pool_gru,
+            x_max_pool_lstm
+        ] + x_pool_cnn
+
+        fm_second = []
+        for t_1, t_2 in itertools.combinations(fm_first, 2):
+            fm_second.append(t_1 * t_2)
+
+        x_fc = self.fc(torch.cat(fm_first + fm_second, 1))
+        x_fc = self.dropout(self.relu(x_fc))
+        outputs = self.output_layer(x_fc)
+        return outputs
 
 
 class StackedCNNRNN(nn.Module):
