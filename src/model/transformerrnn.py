1b40ee7110fbb15f43cc1c88c6853a4f6f55f628
Lain Matsuoka <lain21@mercari.com>
2019-01-23 16:46:50 +0900

    New TransformerRNN model

--- src/model/transformerrnn.py
+++ src/model/transformerrnn.py
@@ -3,17 +3,17 @@ import scipy as sp
 import torch
 import torch.nn as nn
 
+from .common import Attention, Dense
 from .transformer import EncoderUnitLayer, get_sinusoid_encoding_table, get_attn_key_pad_mask, get_non_pad_mask
 
 
 class TransformerRNN(nn.Module):
     def __init__(self, embedding_matrix, seq_length, hidden_dim=64, out_hidden_dim=64, out_drop=0.3, embed_drop=0.2,
-                 num_head=8, k_dim=16, inner_dim=0, trans_drop=0.2, add_position=True):
+                 num_head=8, k_dim=16, inner_dim=0, trans_drop=0.2):
         super(TransformerRNN, self).__init__()
 
         n_position = seq_length + 1
         embed_dim = embedding_matrix.shape[1]
-        self.add_position = add_position
 
         self.word_embedding = nn.Embedding.from_pretrained(torch.FloatTensor(embedding_matrix), freeze=True)
         self.position_embedding = nn.Embedding.from_pretrained(
@@ -21,13 +21,18 @@ class TransformerRNN(nn.Module):
             freeze=True)
         self.embedding_dropout = nn.Dropout2d(embed_drop)
 
+        self.transformer = EncoderUnitLayer(embed_dim, inner_dim=inner_dim, num_head=num_head,
+                                            k_dim=k_dim, v_dim=k_dim, dropout=trans_drop)
+        self.transformer_avg_fc = Dense(embed_dim, hidden_dim * 2, dropout=trans_drop, activation='relu')
+        self.transformer_max_fc = Dense(embed_dim, hidden_dim * 2, dropout=trans_drop, activation='relu')
+
         self.lstm = nn.LSTM(embed_dim, hidden_dim, bidirectional=True, batch_first=True)
         self.gru = nn.GRU(hidden_dim * 2, hidden_dim, bidirectional=True, batch_first=True)
-        self.lstm_attention = EncoderUnitLayer(hidden_dim * 2, inner_dim, num_head, k_dim, k_dim, dropout=trans_drop)
-        self.gru_attention = EncoderUnitLayer(hidden_dim * 2, inner_dim, num_head, k_dim, k_dim, dropout=trans_drop)
+        self.lstm_attention = Attention(hidden_dim * 2, seq_length)
+        self.gru_attention = Attention(hidden_dim * 2, seq_length)
 
-        fm_first_size = hidden_dim * 2 * 4
-        fm_second_size = hidden_dim * 2 * sp.special.comb(4, 2)
+        fm_first_size = hidden_dim * 2 * 8
+        fm_second_size = hidden_dim * 2 * sp.special.comb(8, 2)
 
         self.fc = nn.Linear(int(fm_first_size + fm_second_size), out_hidden_dim)
         self.relu = nn.ReLU()
@@ -42,24 +47,31 @@ class TransformerRNN(nn.Module):
         x_embed = self.word_embedding(seq)
         x_pos = self.position_embedding(pos)
 
-        x_embed = self.embedding_dropout(torch.unsqueeze(x_embed, 0).transpose(1, 3))
-        x_embed = torch.squeeze(x_embed.transpose(1, 3))
+        x_trans = self.transformer(x_embed + x_pos, non_pad_mask, attention_mask)
 
-        x_lstm, _ = self.lstm(x_embed)
+        x_embed_rnn = self.embedding_dropout(torch.unsqueeze(x_embed, 0).transpose(1, 3))
+        x_embed_rnn = torch.squeeze(x_embed_rnn.transpose(1, 3))
+
+        x_lstm, _ = self.lstm(x_embed_rnn)
         x_gru, _ = self.gru(x_lstm)
 
-        if self.add_position:
-            x_lstm += x_pos
-            x_gru += x_pos
-        x_lstm_attention, _ = self.lstm_attention(x_lstm, non_pad_mask, attention_mask)
-        x_gru_attention, _ = self.gru_attention(x_gru, non_pad_mask, attention_mask)
+        x_trans_avg_pool = torch.mean(x_trans, 1)
+        x_trans_max_pool, _ = torch.max(x_trans, 1)
+        x_trans_avg_pool = self.transformer_avg_fc(x_trans_avg_pool)
+        x_trans_max_pool = self.transformer_max_fc(x_trans_max_pool)
 
-        x_avg_pool_lstm = torch.mean(x_lstm_attention, 1)
-        x_max_pool_lstm, _ = torch.max(x_lstm_attention, 1)
-        x_avg_pool_gru = torch.mean(x_gru_attention, 1)
-        x_max_pool_gru, _ = torch.max(x_gru_attention, 1)
+        x_lstm_attention = self.lstm_attention(x_lstm * non_pad_mask)
+        x_gru_attention = self.gru_attention(x_gru * non_pad_mask)
+        x_avg_pool_lstm = torch.mean(x_lstm, 1)
+        x_max_pool_lstm, _ = torch.max(x_lstm, 1)
+        x_avg_pool_gru = torch.mean(x_gru, 1)
+        x_max_pool_gru, _ = torch.max(x_gru, 1)
 
         fm_first = [
+            x_trans_avg_pool,
+            x_trans_max_pool,
+            x_lstm_attention,
+            x_gru_attention,
             x_avg_pool_lstm,
             x_max_pool_lstm,
             x_avg_pool_gru,
