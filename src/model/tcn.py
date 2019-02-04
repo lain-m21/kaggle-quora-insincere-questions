5ac81075900c1d55678561beed03d83f2227656f
Lain Matsuoka <lain21@mercari.com>
2019-02-05 01:41:47 +0900

    num_layers option

--- src/model/tcn.py
+++ src/model/tcn.py
@@ -18,29 +18,34 @@ class Chomp1d(nn.Module):
 
 
 class TemporalConvolutionBlock(nn.Module):
-    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2, num_layers=2):
         super(TemporalConvolutionBlock, self).__init__()
+        self.num_layers = num_layers
         self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
                                            stride=stride, padding=padding, dilation=dilation))
         self.chomp1 = Chomp1d(padding)
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
 
-        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
-                                           stride=stride, padding=padding, dilation=dilation))
-        self.chomp2 = Chomp1d(padding)
-        self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(dropout)
+        if num_layers == 1:
+            self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1)
+        else:
+            self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                               stride=stride, padding=padding, dilation=dilation))
+            self.chomp2 = Chomp1d(padding)
+            self.relu2 = nn.ReLU()
+            self.dropout2 = nn.Dropout(dropout)
 
-        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
-                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+            self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                     self.conv2, self.chomp2, self.relu2, self.dropout2)
         self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
         self.relu = nn.ReLU()
         self.init_weights()
 
     def init_weights(self):
         self.conv1.weight.data.normal_(0, 0.01)
-        self.conv2.weight.data.normal_(0, 0.01)
+        if self.num_layers > 1:
+            self.conv2.weight.data.normal_(0, 0.01)
         if self.downsample is not None:
             self.downsample.weight.data.normal_(0, 0.01)
 
@@ -51,7 +56,7 @@ class TemporalConvolutionBlock(nn.Module):
 
 
 class TemporalConvolutionUnit(nn.Module):
-    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2, num_layers=2):
         super(TemporalConvolutionUnit, self).__init__()
         layers = []
         num_levels = len(num_channels)
@@ -59,9 +64,9 @@ class TemporalConvolutionUnit(nn.Module):
             dilation_size = 2 ** i
             in_channels = num_inputs if i == 0 else num_channels[i-1]
             out_channels = num_channels[i]
-            layers += [TemporalConvolutionBlock(in_channels, out_channels, kernel_size,
-                                                stride=1, dilation=dilation_size,
-                                                padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+            layers += [TemporalConvolutionBlock(in_channels, out_channels, kernel_size, stride=1,
+                                                dilation=dilation_size, padding=(kernel_size-1) * dilation_size,
+                                                dropout=dropout, num_layers=num_layers)]
 
         self.network = nn.Sequential(*layers)
 
